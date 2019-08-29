17e11930b68e337cec4241081e7b730705293073
jiangzheng <jiangzheng1582090156>
2019-08-29 10:52:40 +0800

    添加 tk.Mybatis, pageHelper 并自动生成代码

--- itoken-service-admin/src/main/java/com/jzheng/itoken/service/admin/ServiceAdminApplication.java
+++ itoken-service-admin/src/main/java/com/jzheng/itoken/service/admin/ServiceAdminApplication.java
@@ -3,9 +3,13 @@ package com.jzheng.itoken.service.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan(basePackages = "com.jzheng.itoken.service.admin.mapper")
 public class ServiceAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceAdminApplication.class, args);
---
+++ itoken-service-admin/src/main/java/com/jzheng/itoken/service/admin/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package com.jzheng.itoken.service.admin.mapper;
+
+import com.jzheng.itoken.service.admin.domain.TbSysUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
